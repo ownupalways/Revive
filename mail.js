6c8576f34965bbd73242766062e0c4ce76f4380d
Oluwadipe Godwin Jesuropo <65556599+ownupalways@users.noreply.github.com>
2022-11-29 09:41:47 -0800

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,14 +1,6 @@
 const nodemailer = require('nodemailer');
 const mailGun = require('nodemailer-mailgun-transport');
 
-
-const auth = {
-    auth: {
-        api_key: '6ec0b85afa581e086407228a4984739e-100b5c8d-f5792037',
-        domain: 'sandbox73d7c79829b44de599a72e8f439002a5.mailgun.org'
-    }
-};
-
 const transporter = nodemailer.createTransport(mailGun(auth));
 
 const sendMail = (email, subject, text, cb) => {
